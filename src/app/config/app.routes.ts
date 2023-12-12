3137c068cdd37df44b3632707d2d7203f2f1f993
Chris-Am <c.aranguizm@outlook.com>
2023-12-12 17:23:50 -0300

    :sparkles: declaring home component and adding it to routes

--- src/app/config/app.routes.ts
+++ src/app/config/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../presentation/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home-list',
+    component: HomeComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/home-list',
+    pathMatch: 'full',
+  },
+];
--- src/app/presentation/home/home.component.ts
+++ src/app/presentation/home/home.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
