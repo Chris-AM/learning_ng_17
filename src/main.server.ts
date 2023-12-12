275c5936914b1c01599144ce51562883cd771652
Chris-Am <c.aranguizm@outlook.com>
2023-12-12 12:05:22 -0300

    :construction: declaring presentation layer

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app/presentation/app.component';
 import { config } from './app/config/app.config.server';
 
 const bootstrap = () => bootstrapApplication(AppComponent, config);
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/config/app.config';
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app/presentation/app.component';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
