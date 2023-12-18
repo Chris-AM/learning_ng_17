b3bacb723d7ca9cba6aac1a9b456e53546ea80e4
Chris-Am <c.aranguizm@outlook.com>
2023-12-18 10:18:16 -0300

    :sparkles: adding routing to details page

--- src/app/presentation/home/housing-location/housing-location.component.ts
+++ src/app/presentation/home/housing-location/housing-location.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { HousingLocation } from '../../../domain/interfaces/housing-location';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css',
 })
