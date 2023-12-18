65add276caa5bf7bb2df935af1f05022b9e91b92
Chris-Am <c.aranguizm@outlook.com>
2023-12-18 10:16:47 -0300

    :sparkles: adding routing to home page

--- src/app/presentation/shared/header/header.component.ts
+++ src/app/presentation/shared/header/header.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
